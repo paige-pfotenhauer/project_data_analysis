33f7fc02a97c668aa2f219325ea7ee70476b24b9
Paige Schiebel <paige.e.schiebel@vanderbilt.edu>
2024-09-26 09:53:35 -0500

    Moved the column names to the header and renamed the first column WT_191

--- project_code.R
+++ project_code.R
@@ -1,1 +1,18 @@
+#install.packages("pacman")
+#library(pacman)
+pacman::p_load(tidyverse, 
+               devtools,
+               janitor,
+               dplyr)
+
+#install_github("mvuorre/exampleRPackage")
+#help(janitor)
+
 dat <- read.table("GSE203554_borchelt.final.txt")
+
+#creating a tibble and moving column names to the header
+genotyping_tibble <- read.table("GSE203554_borchelt.final.txt", header = TRUE)
+
+#renaming the header names
+genotyping_tibble_headernames <- rename(genotyping_tibble, WT_1 = WT_191)
+
